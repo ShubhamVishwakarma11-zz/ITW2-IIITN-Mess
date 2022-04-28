ca40e68eee62f8322b68d4b427f8fadb85c9103d
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-04-29 00:26:42 +0530

    Add start script

--- index.js
+++ index.js
@@ -258,6 +258,8 @@ app.post('/login', async (req, res) => {
     }
 })
 
-app.listen(3000, function() {
-    console.log("server listening at port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port , function() {
+    console.log(`server listening at port 3000 ${port} `);
 });
